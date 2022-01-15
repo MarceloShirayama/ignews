d1d0bbacb9db5abbe532bec47f81fe8707a8658d
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-01-15 09:20:14 -0300

    add dynamic title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @next/next/no-title-in-document-head */
-// pages/_document.js
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
@@ -13,7 +11,6 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
             rel="stylesheet"
           />
-          <title>ig.news</title>
         </Head>
         <body>
           <Main />
