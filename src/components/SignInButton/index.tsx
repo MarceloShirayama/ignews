0014b366735b874bb5dd9fc84dbfe984412406cb
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-01-15 18:25:52 -0300

    nextauth social authentication on github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,26 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { useSession, signIn, signOut } from "next-auth/react";
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  const { data: session, status } = useSession();
+  return status === "authenticated" ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Marcelo Shirayama
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eab417" />
       Sign in with GitHub
     </button>
