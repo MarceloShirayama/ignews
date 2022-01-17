ec84be15c5e2a0111dcf6824ea234e7a64d1c792
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-01-17 18:13:34 -0300

    add post page layout

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,49 @@
+import Head from "next/head";
+import styles from "./styles.module.scss";
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>17 de janeiro de 2022</time>
+            <strong>
+              Obtendo o status de progresso do envio de dados com Axios
+            </strong>
+            <p>
+              Vamos mostrar na prática como obter o progresso de cada requisição
+              HTTP sendo feita através do método POST, do front end para o back
+              end utilizando o Axios.
+            </p>
+          </a>
+          <a href="#">
+            <time>17 de janeiro de 2022</time>
+            <strong>
+              Obtendo o status de progresso do envio de dados com Axios
+            </strong>
+            <p>
+              Vamos mostrar na prática como obter o progresso de cada requisição
+              HTTP sendo feita através do método POST, do front end para o back
+              end utilizando o Axios.
+            </p>
+          </a>
+          <a href="#">
+            <time>17 de janeiro de 2022</time>
+            <strong>
+              Obtendo o status de progresso do envio de dados com Axios
+            </strong>
+            <p>
+              Vamos mostrar na prática como obter o progresso de cada requisição
+              HTTP sendo feita através do método POST, do front end para o back
+              end utilizando o Axios.
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
