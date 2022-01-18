b2821fc05a5b0ad7cee83b8614c059077efdcaeb
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-01-18 16:54:08 -0300

    fix: error instance not found

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,29 +13,36 @@ export default NextAuth({
   ],
   callbacks: {
     async session({ session }) {
-      const userActiveSubscription = await faunaClient.query(
-        query.Get(
-          query.Intersection([
-            query.Match(
-              query.Index("subscription_by_user_ref"),
-              query.Select(
-                "ref",
-                query.Get(
-                  query.Match(
-                    query.Index("user_by_email"),
-                    query.Casefold(session.user.email)
+      try {
+        const userActiveSubscription = await faunaClient.query(
+          query.Get(
+            query.Intersection([
+              query.Match(
+                query.Index("subscription_by_user_ref"),
+                query.Select(
+                  "ref",
+                  query.Get(
+                    query.Match(
+                      query.Index("user_by_email"),
+                      query.Casefold(session.user.email)
+                    )
                   )
                 )
-              )
-            ),
-            query.Match(query.Index("subscription_by_status"), "active"),
-          ])
-        )
-      );
-      return {
-        ...session,
-        activeSubscription: userActiveSubscription,
-      };
+              ),
+              query.Match(query.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
     },
 
     async signIn({ user }) {
