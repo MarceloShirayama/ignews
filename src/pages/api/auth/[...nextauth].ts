8bbdd7634fb0174a7f6a74b8b878babda7b8bf11
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-01-18 16:41:01 -0300

    add sessioncalback in NextAuth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -12,6 +12,32 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session({ session }) {
+      const userActiveSubscription = await faunaClient.query(
+        query.Get(
+          query.Intersection([
+            query.Match(
+              query.Index("subscription_by_user_ref"),
+              query.Select(
+                "ref",
+                query.Get(
+                  query.Match(
+                    query.Index("user_by_email"),
+                    query.Casefold(session.user.email)
+                  )
+                )
+              )
+            ),
+            query.Match(query.Index("subscription_by_status"), "active"),
+          ])
+        )
+      );
+      return {
+        ...session,
+        activeSubscription: userActiveSubscription,
+      };
+    },
+
     async signIn({ user }) {
       const { email } = user;
       try {
