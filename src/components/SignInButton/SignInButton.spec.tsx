0f37475730b36e6b8028d26bdb2ab2069328c3f6
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-02-06 12:23:59 -0300

    ensure SignInButton renders correctly when the user is not authenticated

---
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import { useSession } from "next-auth/react";
+import { MockedFunction } from "ts-jest";
+import { SignInButton } from ".";
+
+jest.mock("next-auth/react");
+
+describe("SignInButton component", () => {
+  it("renders correctly when user is not authenticated", () => {
+    const mockUseSession: MockedFunction<typeof useSession> = useSession as any;
+    mockUseSession.mockReturnValueOnce({
+      data: null,
+      status: "loading",
+    });
+
+    render(<SignInButton />);
+
+    expect(screen.getByText("Sign in with GitHub")).toBeInTheDocument();
+  });
+});
