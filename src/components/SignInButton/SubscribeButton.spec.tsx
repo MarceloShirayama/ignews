56da718d4c718471c1c2ea169d80c783c5ae9319
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-02-06 14:26:38 -0300

    ensure SubscribeButton redirect to signIn route when user is not logged in

--- src/components/SignInButton/SubscribeButton.spec.tsx
+++ src/components/SignInButton/SubscribeButton.spec.tsx
@@ -1,5 +1,5 @@
-import { render, screen } from "@testing-library/react";
-import { useSession } from "next-auth/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { signIn, useSession } from "next-auth/react";
 import { MockedFunction } from "ts-jest";
 import { SubscribeButton } from "../SubscribeButton";
 
@@ -16,5 +16,25 @@ describe("SubscribeButton component", () => {
     render(<SubscribeButton />);
 
     expect(screen.getByText("Subscribe now")).toBeInTheDocument();
+  });
+
+  it("redirect to signIn route when user is not logged in", () => {
+    const mockUseSession: MockedFunction<typeof useSession> = useSession as any;
+    mockUseSession.mockReturnValueOnce({
+      data: null,
+      status: "loading",
+    });
+
+    const mockSignIn: MockedFunction<typeof signIn> = signIn as any;
+
+    render(<SubscribeButton />);
+
+    const subscribeButton = screen.getByText("Subscribe now");
+
+    fireEvent.click(subscribeButton);
+
+    expect(mockSignIn).toHaveBeenCalled();
+    expect(mockSignIn).toHaveBeenCalledTimes(1);
+    expect(mockSignIn).toHaveBeenCalledWith("github");
   });
 });
