1facd7fc3deedee318b39f1fc4753299ef7d0b4b
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-02-06 19:25:11 -0300

    ensure Home page loads initial data with getStaticProps

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,5 +1,9 @@
 import { render, screen } from "@testing-library/react";
-import Home from "../../pages";
+import { mocked } from "ts-jest/dist/utils/testing";
+import Home, { getStaticProps } from "../../pages";
+import { stripe } from "../../services/stripe";
+
+jest.mock("../../services/stripe");
 
 jest.mock("next-auth/react", () => {
   return {
@@ -23,5 +27,30 @@ describe("Home page", () => {
     expect(regularExpression).toBeInTheDocument();
     expect(screen.getByText("Subscribe now")).toBeInTheDocument();
     expect(image.src).toContain("/images/avatar.svg");
+  });
+
+  it("loads initial data with getStaticProps", async () => {
+    const mockedRetrievePricesStripe = mocked(stripe.prices.retrieve);
+    const id = "fake-price-id";
+    const unit_amount = 1000;
+
+    mockedRetrievePricesStripe.mockResolvedValue({
+      id,
+      unit_amount,
+    } as any);
+
+    const result = await getStaticProps({});
+    console.log(result);
+
+    expect(result).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: id,
+            amount: `$${unit_amount / 100}.00`,
+          },
+        },
+      })
+    );
   });
 });
