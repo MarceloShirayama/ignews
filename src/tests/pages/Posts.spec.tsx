ca062df280749865d0078c4eee827fd018133599
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-02-06 22:38:19 -0300

    ensure Posts page loads initial data with getStaticProps

--- src/tests/pages/Posts.spec.tsx
+++ src/tests/pages/Posts.spec.tsx
@@ -1,14 +1,27 @@
 import { render, screen } from "@testing-library/react";
-import Posts from "../../pages/posts";
+import { mocked } from "ts-jest/dist/utils/testing";
+import Posts, { getStaticProps } from "../../pages/posts";
+import { getPrismicClient } from "../../services/prismic";
 
 const fakePosts = [
   {
-    slug: "fake-slug",
-    title: "fake-title",
+    slug: "my-new-post",
+    title: "My New Post",
     excerpt: "fake-excerpt",
-    updatedAt: "fake-updated-at",
+    updatedAt: "10 de Abril",
   },
 ];
+const fakeLastPublicationDate = "04-01-2022";
+
+const fakeLastPublicationDateFormatted = new Date(
+  fakeLastPublicationDate
+).toLocaleDateString("pt-BR", {
+  day: "2-digit",
+  month: "long",
+  year: "numeric",
+});
+
+jest.mock("../../services/prismic");
 
 describe("Posts page", () => {
   it("renders correctly", () => {
@@ -20,6 +33,42 @@ describe("Posts page", () => {
     expect(screen.getByRole("link")).toHaveAttribute(
       "href",
       `/posts/${fakePosts[0].slug}`
+    );
+  });
+
+  it("loads initial data with getStaticProps", async () => {
+    const mockedGetPrismicClient = mocked(getPrismicClient);
+
+    mockedGetPrismicClient.mockReturnValueOnce({
+      query: jest.fn().mockResolvedValueOnce({
+        results: [
+          {
+            uid: fakePosts[0].slug,
+            data: {
+              title: [{ type: "heading", text: fakePosts[0].title }],
+              content: [{ type: "paragraph", text: fakePosts[0].excerpt }],
+            },
+            last_publication_date: fakeLastPublicationDate,
+          },
+        ],
+      }),
+    } as any);
+
+    const result = await getStaticProps({});
+
+    expect(result).toEqual(
+      expect.objectContaining({
+        props: {
+          posts: [
+            {
+              slug: fakePosts[0].slug,
+              title: fakePosts[0].title,
+              excerpt: fakePosts[0].excerpt,
+              updatedAt: fakeLastPublicationDateFormatted,
+            },
+          ],
+        },
+      })
     );
   });
 });
