0299da0948394cfce655920d519b62f9bd16d005
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-02-07 15:14:13 -0300

    remove console.log

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -5,7 +5,6 @@ import styles from "./styles.module.scss";
 
 export function SignInButton() {
   const { data: session, status } = useSession();
-  console.log(useSession());
 
   return status === "authenticated" ? (
     <button
