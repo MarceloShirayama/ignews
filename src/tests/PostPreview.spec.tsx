8f4e22f280470da0e4448f1891c8402c5708ce25
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-02-07 16:18:48 -0300

    ensure PostPreview page redirects user to full post when active subscription

--- src/tests/PostPreview.spec.tsx
+++ src/tests/PostPreview.spec.tsx
@@ -1,9 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import { MockedFunction } from "ts-jest/dist/utils/testing";
 import PostPreview from "../pages/posts/preview/[slug]";
 
 jest.mock("next-auth/react");
+jest.mock("next/router");
 
 const postFake = {
   slug: "fake-post-slug",
@@ -11,6 +13,8 @@ const postFake = {
   content: "fake-post-content",
   updatedAt: "fake-post-updatedAt",
 };
+
+const activeSubscription = "fake-active-subscription";
 
 describe("PostPreview page", () => {
   it("renders correctly", () => {
@@ -26,5 +30,23 @@ describe("PostPreview page", () => {
     expect(screen.getByText(postFake.updatedAt)).toBeInTheDocument();
     expect(screen.getByText(postFake.content)).toBeInTheDocument();
     expect(screen.getByText("Wanna continue reading?")).toBeInTheDocument();
+  });
+
+  it("redirects user to full post when active subscription", async () => {
+    const mockUseSession: MockedFunction<typeof useSession> = useSession as any;
+    const mockUseRouter: MockedFunction<typeof useRouter> = useRouter as any;
+    const mockPush = jest.fn();
+
+    mockUseSession.mockReturnValueOnce({
+      data: { activeSubscription },
+    } as any);
+
+    mockUseRouter.mockReturnValueOnce({
+      push: mockPush,
+    } as any);
+
+    render(<PostPreview post={postFake} />);
+
+    expect(mockPush).toHaveBeenCalledWith(`/posts/${postFake.slug}`);
   });
 });
